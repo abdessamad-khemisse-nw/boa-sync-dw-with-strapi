6e2eaa7b69dac919b8b0fa24a1f09a0167e2b0e1
Khemisse-Abdessamad-NW <abdessamad.khemisse@nimbleways.com>
2024-06-11 17:35:31 +0100

    remove options from graphql client

--- utils/graphql.ts
+++ utils/graphql.ts
@@ -1,6 +1,5 @@
-import {GraphQLClient} from "graphql-request";
+import { GraphQLClient } from 'graphql-request';
 
 const endpoint = 'http://localhost:1331/graphql';
 
-export const client = new GraphQLClient(endpoint, {
-});
+export const client = new GraphQLClient(endpoint);
