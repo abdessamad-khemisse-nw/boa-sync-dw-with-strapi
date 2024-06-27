b5ede5e242f62c436ea74de3168e95f674c4cad9
mohamed-ahrrass-nw <mohamed.ahrrass@nimbleways.com>
2024-06-27 10:17:09 +0100

    Enable eslint max-lines rule

--- script.ts
+++ script.ts
@@ -20,7 +20,7 @@ config.queries.forEach((queryConfig) => {
         useWordingQueryOrDefault(queriesMap[queryName](language), language, config.mediaDir).then(
             (data) => {
                 const filePath = `${queryPath}/${language}.ts`;
-                const fileContent = `/* eslint-disable max-lines */\nexport const ${queryName}${language.toUpperCase()} = ${JSON.stringify(data)}`;
+                const fileContent = `export const ${queryName}${language.toUpperCase()} = ${JSON.stringify(data)}`;
                 fs.mkdirSync(queryPath, { recursive: true });
 
                 fs.writeFile(filePath, fileContent, (err) => {
