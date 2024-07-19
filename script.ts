e4ca74b482d35a00a65ae51a5e424d12c016992f
tarraqmo <107695899+tarraqmo@users.noreply.github.com>
2024-07-19 09:16:39 +0100

    Update script.ts

--- script.ts
+++ script.ts
@@ -39,6 +39,7 @@ const queriesMap = {
   ClientInfoStepQuery,
   FAQPageQuery,
   BankCardsPageQuery,
+  SeoOpenAccountQuery,
   ConventionWordingQuery,
   SatisfactionSurveyPageQuery,
   NotificationsQuery,
